1d48f7da1f9efcb6ca719229d5c37447dc3804bd
Tyler Jones <tyler@gumstix.com>
2012-10-29 15:13:46 -0700

    Updated xmonad colors and startup app. Added natural scrolling to Xmodmap.

--- dot-home/xmonad/xmonad.hs
+++ dot-home/xmonad/xmonad.hs
@@ -5,10 +5,11 @@ import XMonad.ManageHook
 main = xmonad gnomeConfig {
     manageHook = myManageHook
     , startupHook = startup
+    , terminal = "xterm"
+    , borderWidth = 1
+    , normalBorderColor  = "#000"
+    , focusedBorderColor = "#FF5721"
     }
-
-terminal :: String
-terminal = "xterm"
 
 myManageHook = composeAll (
     [ manageHook gnomeConfig
@@ -17,7 +18,11 @@ myManageHook = composeAll (
 
 startup :: X ()
 startup = do
-          spawn "gnome-do"
+          spawn "gnome-keyring-daemon"
+          spawn "gnome-screensaver"
+          spawn "gnome-settings-daemon"
+          spawn "gnome-power-manager"
+          spawn "/home/tyler/.startup"
           spawn "ssh-add"
+          spawn "gnome-do"
           spawn "parcellite"
-          spawn "/home/tyler/.screenlayout/autostart.sh"
