7eaa2bf9fa476e5d9071cb9a012f871e41c1485c
Tyler Jones <tyler@squirly.ca>
2015-10-29 13:16:09 -0700

    Make xmonad work with Intellij Idea IDE

--- dot-home/xmonad/xmonad.hs
+++ dot-home/xmonad/xmonad.hs
@@ -3,15 +3,20 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.ManageHook
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.CopyWindow
+import Data.Map as Map
 import Data.List
+
+q ~=? x = fmap (isPrefixOf x) q
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/tyler/.xmonad/xmobar"
 
     xmonad $ ewmh defaultConfig {
-          manageHook = manageDocks <+> myManageHook
+          manageHook = myManageHook
 	, layoutHook = avoidStruts $ layoutHook defaultConfig
         , startupHook = startup
+        , XMonad.keys = myKeys  <+> XMonad.keys defaultConfig
         , terminal = "xterm"
         , borderWidth = 1
         , normalBorderColor  = "#000"
@@ -23,6 +28,12 @@ myManageHook = composeAll [
     , className =? "Do"   --> doFloat
     , className =? "Gimp" --> doFloat
     , className =? "Vncviewer" --> doFloat
+    , (className ~=? "jetbrains-") <&&> (title ~=? "win") --> doIgnore
+    ] <+> manageDocks
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = Map.fromList [
+      ((modm .|. controlMask, xK_a ), windows copyToAll) -- @@ Make focused window always visible
+    , ((modm .|. controlMask .|. shiftMask, xK_a ),  killAllOtherCopies) -- @@ Toggle window state back
     ]
 
 startup :: X ()
