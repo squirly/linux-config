f8350aff6f845c40cef8179e4fd2168aeeb3b578
Tyler Jones <tyler@squirly.ca>
2017-12-01 21:53:26 -0800

    New xmonad config

--- dot-home/xmonad/xmonad.hs
+++ dot-home/xmonad/xmonad.hs
@@ -1,49 +1,61 @@
 import XMonad
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.ManageHook
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.CopyWindow
-import Data.Map as Map
-import Data.List
+import XMonad.Config.Gnome
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import System.Exit
 
-q ~=? x = fmap (isPrefixOf x) q
+import qualified Data.Map        as M
+import qualified Data.List       as L
+import qualified XMonad.StackSet as W
 
-main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/tyler/.xmonad/xmobar"
+q ~=? x = fmap (L.isPrefixOf x) q
 
-    xmonad $ ewmh defaultConfig {
-          manageHook = myManageHook
-	, layoutHook = avoidStruts $ layoutHook defaultConfig
-        , startupHook = startup
-        , XMonad.keys = myKeys  <+> XMonad.keys defaultConfig
-        , terminal = "xterm"
-        , borderWidth = 1
-        , normalBorderColor  = "#000"
-        , focusedBorderColor = "#FF5721"
-    }
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [
+      ((modm              ,  xK_k ), windows W.focusDown     )
+    , ((modm              ,  xK_j ), windows W.focusUp       )
+    , ((modm .|. shiftMask,  xK_k ), windows W.swapDown      )
+    , ((modm .|. shiftMask,  xK_j ), windows W.swapUp        )
+    , ((modm              ,  xK_a ), windows copyToAll       )
+    , ((modm .|. shiftMask,  xK_a ), killAllOtherCopies      )
+    , ((modm              ,  xK_b ), sendMessage ToggleStruts)
+  ]
 
+myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
+    where tiled = Tall 1 (2/100) (4/7)
+
+-- Use `xprop | grep WM_CLASS` for 'className' and 'resource'
+-- Use `xprop | grep WM_NAME` for 'title'
 myManageHook = composeAll [
-      manageHook defaultConfig
-    , resource =? "synapse" --> doIgnore
-    , className =? "Gimp" --> doFloat
-    , className =? "Vncviewer" --> doFloat
+      className =? "Cerebro"        --> doCenterFloat
+    , className =? "Gimp"           --> doFloat
+    , className =? "Vncviewer"      --> doFloat
     , (className ~=? "jetbrains-") <&&> (title ~=? "win") --> doIgnore
-    ] <+> manageDocks
+    , resource  =? "desktop_window" --> doIgnore
+  ]
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = Map.fromList [
-      ((modm .|. controlMask, xK_a ), windows copyToAll) -- @@ Make focused window always visible
-    , ((modm .|. controlMask .|. shiftMask, xK_a ),  killAllOtherCopies) -- @@ Toggle window state back
-    ]
+myLogHook = return ()
 
-startup :: X ()
-startup = do
-          spawn "wmname LG3D"
-          spawn "/usr/bin/gnome-keyring-daemon --start --components=pkcs11"
-          spawn "xloadimage -onroot -fullscreen ~/.background"
-          spawn "gnome-screensaver"
-          spawn "gnome-session"
-          spawn "unity-settings-daemon"
-          spawn "~/.startup"
-          spawn "ssh-add"
-          spawn "synapse"
+myStartupHook = do
+    -- spawn "wmname LG3D"
+    spawn "gnome-keyring-daemon --start --components=pkcs11"
+    spawn "xloadimage -onroot -center -zoom 80 ~/.background"
+    spawn "gnome-screensaver"
+    spawn "gnome-session"
+    spawn "~/.startup"
+    spawn "ssh-add"
+    spawn "cerebro"
+
+main =
+    xmonad $ gnomeConfig {
+        terminal           = "xterm",
+        focusFollowsMouse  = True,
+        borderWidth        = 1,
+        normalBorderColor  = "black",
+        focusedBorderColor = "#FF5721",
+        keys               = myKeys <+> XMonad.keys defaultConfig,
+        layoutHook         = myLayout,
+        manageHook         = myManageHook,
+        logHook            = myLogHook,
+        startupHook        = myStartupHook
+    }
