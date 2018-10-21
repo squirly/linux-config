fade7f0ad49f13d414b93ebe31d175cc0ba7813b
Tyler Jones <tylerjones64@gmail.com>
2018-10-21 10:09:22 -0700

    improve xmonad config

--- dot-home/xmonad/xmonad.hs
+++ dot-home/xmonad/xmonad.hs
@@ -1,10 +1,13 @@
 import XMonad
 import XMonad.Actions.CopyWindow
+-- import XMonad.Actions.Volume
 import XMonad.Config.Gnome
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Util.Run
 import System.Exit
-
 import qualified Data.Map        as M
 import qualified Data.List       as L
 import qualified XMonad.StackSet as W
@@ -12,13 +15,18 @@ import qualified XMonad.StackSet as W
 q ~=? x = fmap (L.isPrefixOf x) q
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [
-      ((modm              ,  xK_k ), windows W.focusDown     )
+      ((modm              ,  xK_x ), kill                    )
+    , ((modm              ,  xK_k ), windows W.focusDown     )
     , ((modm              ,  xK_j ), windows W.focusUp       )
     , ((modm .|. shiftMask,  xK_k ), windows W.swapDown      )
     , ((modm .|. shiftMask,  xK_j ), windows W.swapUp        )
     , ((modm              ,  xK_a ), windows copyToAll       )
     , ((modm .|. shiftMask,  xK_a ), killAllOtherCopies      )
     , ((modm              ,  xK_b ), sendMessage ToggleStruts)
+    , ((mod1Mask          ,  xK_space), spawn "synapse"      )
+    -- , ((modm              , xK_F1 ), lowerVolume 3 >> return ())
+    -- , ((modm              , xK_F2 ), raiseVolume 3 >> return ())
+    -- , ((modm              , xK_F3 ), toggleMute    >> return ())
   ]
 
 myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
@@ -32,22 +40,26 @@ myManageHook = composeAll [
     , className =? "Vncviewer"      --> doFloat
     , (className ~=? "jetbrains-") <&&> (title ~=? "win") --> doIgnore
     , resource  =? "desktop_window" --> doIgnore
+    , resource  =? "synapse" --> doIgnore
   ]
 
 myLogHook = return ()
 
 myStartupHook = do
-    -- spawn "wmname LG3D"
+    setWMName "LG3D"
     spawn "gnome-keyring-daemon --start --components=pkcs11"
     spawn "xloadimage -onroot -center -zoom 80 ~/.background"
-    spawn "gnome-screensaver"
-    spawn "gnome-session"
+    -- spawn "gnome-screensaver"
+    -- spawn "gnome-session"
+    spawn "unity-settings-daemon"
     spawn "~/.startup"
     spawn "ssh-add"
-    spawn "cerebro"
+    spawn "parcellite"
 
-main =
+main = do
+    xmobar <- spawnPipe "xmobar ~/.xmonad/xmobar"
     xmonad $ gnomeConfig {
+        modMask            = mod4Mask,
         terminal           = "xterm",
         focusFollowsMouse  = True,
         borderWidth        = 1,
@@ -57,5 +69,6 @@ main =
         layoutHook         = myLayout,
         manageHook         = myManageHook,
         logHook            = myLogHook,
-        startupHook        = myStartupHook
+        startupHook        = myStartupHook,
+        handleEventHook    = fullscreenEventHook
     }
